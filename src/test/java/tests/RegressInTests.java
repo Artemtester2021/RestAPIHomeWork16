82c381cc231896f2f18ef1d050151b1fc87ac054
Artem <artem@gmail.com>
2025-06-12 10:07:04 +0300

    added lombok

--- src/test/java/tests/RegressInTests.java
+++ src/test/java/tests/RegressInTests.java
@@ -1,17 +1,22 @@
 package tests;
 
+import io.qameta.allure.restassured.AllureRestAssured;
+import models.lombok.DataBodyLombokModel;
+import models.lombok.DataResponseLombokModel;
 import models.pojo.DataBodyModel;
 import models.pojo.DataResponseModel;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static helpers.CustomAllureListener.withCustomTemplates;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static java.net.HttpURLConnection.*;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
+@Tag("restApi")
 public class RegressInTests extends TestBase {
 
     int validUserId = 2;
@@ -20,21 +25,24 @@ public class RegressInTests extends TestBase {
 
     @Test
     @DisplayName("Запрос данных по несуществующему пользователю")
-    void singleUserNotFoundTest(){
+    void singleUserNotFoundTest() {
         given()
                 .header(TestBase.FREE_API_KEY_NAME, TestBase.FREE_API_KEY_VALUE)
+                .filter(withCustomTemplates())
                 .log().uri()
                 .when()
                 .get(TestBase.USERS_END_POINT + notValidUserId)
                 .then()
                 .log().status()
+                .log().body()
+                .log().headers()
                 .statusCode(HTTP_NOT_FOUND)
                 .body(is("{}"));
     }
 
     @Test
     @DisplayName("Ни один ресурс не найден")
-    void SingleResourceNotFound(){
+    void SingleResourceNotFound() {
         given()
                 .header(TestBase.FREE_API_KEY_NAME, TestBase.FREE_API_KEY_VALUE)
                 .log().uri()
@@ -42,17 +50,19 @@ public class RegressInTests extends TestBase {
                 .get(TestBase.RESOURS_END_POINT + notResourceUserId)
                 .then()
                 .log().status()
+                .log().body()
+                .log().headers()
                 .statusCode(HTTP_NOT_FOUND)
                 .body(is("{}"));
     }
 
     @Test
     @DisplayName("Обновление имени пользователя (через put)")
-    void updateTest(){
-        DataBodyModel bodyDate = new DataBodyModel();
+    void updateLombokTest() {
+        DataBodyLombokModel bodyDate = new DataBodyLombokModel();
         bodyDate.setName("morpheus");
 
-        DataResponseModel response = given()
+        DataResponseLombokModel response = given()
                 .header(TestBase.FREE_API_KEY_NAME, TestBase.FREE_API_KEY_VALUE)
                 .body(bodyDate)
                 .contentType(JSON)
@@ -61,16 +71,17 @@ public class RegressInTests extends TestBase {
                 .put(TestBase.USERS_END_POINT + validUserId)
                 .then()
                 .log().status()
+                .log().body()
+                .log().headers()
                 .statusCode(HTTP_OK)
-                .extract().as(DataResponseModel.class);
+                .extract().as(DataResponseLombokModel.class);
 
         assertEquals("morpheus", response.getName());
     }
 
     @Test
     @DisplayName("Изменение должности пользователя (через patch)")
-    void successfulPatchUserJobTest() {
-//        String newUserDataBody = "{\"job\": \"zion resident\"}";
+    void successfulPatchUserJobPojoTest() {
         DataBodyModel bodyDate = new DataBodyModel();
         bodyDate.setName("zion resident");
 
@@ -84,6 +95,7 @@ public class RegressInTests extends TestBase {
                 .then()
                 .log().status()
                 .log().body()
+                .log().headers()
                 .statusCode(HTTP_OK)
                 .extract().as(DataResponseModel.class);
 
@@ -101,6 +113,8 @@ public class RegressInTests extends TestBase {
                 .delete(TestBase.USERS_END_POINT + validUserId)
                 .then()
                 .log().status()
+                .log().body()
+                .log().headers()
                 .statusCode(HTTP_NO_CONTENT);
     }
 }
